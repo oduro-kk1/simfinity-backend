7a626bd12b47dbee3f88aac59d54b39f1622e6fc
Mic <obenefour2120@gmail.com>
2026-06-30 23:27:42 +0000

    Add Swagger + HelloController

--- src/main/java/com/simfinity/backend/Controller/HelloController.java
+++ src/main/java/com/simfinity/backend/Controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.simfinity.backend.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Simfinity backend is alive!";
+    }
 }
