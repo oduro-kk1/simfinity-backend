9493c862ee45be6f4ff0c134f7ba756d026aefcb
Mic <obenefour2120@gmail.com>
2026-07-02 03:35:35 +0000

    Force scan controller package

--- src/main/java/com/simfinity/backend/SimfinityBackendApplication.java
+++ src/main/java/com/simfinity/backend/SimfinityBackendApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.simfinity.backend"})
 @EnableAsync
 public class SimfinityBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimfinityBackendApplication.class, args);
 	}
-
 }
-
-
-
